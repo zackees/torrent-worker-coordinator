95711be3af5f3e49f599bab3bfd1204e8191af00
zackees <z@zackees.com>
2024-12-09 16:04:49 -0800

    fix: reduce uvicorn workers from 8 to 1

--- entry_point.sh
+++ entry_point.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-uvicorn --host 0.0.0.0 --port 80 --workers 8 --forwarded-allow-ips=* torrent_worker_coordinator.app:app
+uvicorn --host 0.0.0.0 --port 80 --workers 1 --forwarded-allow-ips=* torrent_worker_coordinator.app:app
