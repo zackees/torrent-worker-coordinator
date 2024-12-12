9f59e8ccff71663932bf950b20222d1ed53b5c49
zackees <z@zackees.com>
2024-12-12 06:52:04 -0800

    refactor(test): use random port for test server initialization

--- src/torrent_worker_coordinator/test/test_app.py
+++ src/torrent_worker_coordinator/test/test_app.py
@@ -6,16 +6,24 @@ Common code for integration tests.
 # pylint: disable=wrong-import-position,too-many-public-methods,unused-import,self-assigning-variable
 
 
+import random
+
 from torrent_worker_coordinator.client import Client
 from torrent_worker_coordinator.settings import API_KEY
 from torrent_worker_coordinator.test.run_server_in_thread import run_server_in_thread
+
+
+def _get_next_port() -> int:
+
+    random_port = random.randint(5000, 9000)
+    return random_port
 
 
 class TestApp(Client):
     __test__ = False  # Prevent discovery
 
     def __init__(self, port: int, api_key: str = API_KEY) -> None:
-        super().__init__(host="localhost", port=port, api_key=api_key)
+        super().__init__(host="localhost", port=_get_next_port(), api_key=api_key)
         self.api_key = api_key
         self.server_context = None
 
